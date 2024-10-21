julianlam/open-tacos

commit fdb64aa049e37ee0128942d0e21ca3982e1d28ac

refactor: use flex to prevent overlap of title to close button (#1148)

src/components/ui/MobileDialog.tsx
@@ -22,15 +22,16 @@ export const DialogContent = React.forwardRef<any, Props>(
           data-no-dnd='true'
           className={clx(fullScreen ? 'dialog-wide' : 'dialog-default')} {...props} ref={forwardedRef}
         >
-          <DialogPrimitive.Title className='dialog-title'>
-            {title}
-          </DialogPrimitive.Title>
-          {/* Use absolute positioning to place the close button on the upper left corner */}
-          <DialogPrimitive.Close aria-label='Close' asChild className='dialog-close-button'>
-            <button className='btn btn-circle btn-ghost outline-none'>
-              <XMarkIcon size={24} />
-            </button>
-          </DialogPrimitive.Close>
+          <div className='flex items-center px-2'>
+            <DialogPrimitive.Close aria-label='Close' asChild className='dialog-close-button'>
+              <button className='btn btn-circle btn-ghost outline-none'>
+                <XMarkIcon size={24} />
+              </button>
+            </DialogPrimitive.Close>
+            <DialogPrimitive.Title className='dialog-title'>
+              {title}
+            </DialogPrimitive.Title>
+          </div>
           {children}
         </DialogPrimitive.Content>
       </DialogPrimitive.Portal>

src/app/global.css
@@ -114,7 +114,7 @@ A slightly deemphasized dotted underline for a tag in order to not competing wit
   }
 
   .dialog-close-button {
-    @apply absolute top-1 left-1 z-50;
+    @apply mx-auto z-auto;
   }
 
   .dialog-form-default {
@@ -122,7 +122,7 @@ A slightly deemphasized dotted underline for a tag in order to not competing wit
   }
 
   .dialog-title {
-    @apply py-2 w-full text-center bg-base-100 z-50 leading-none;
+    @apply flex-grow text-center bg-base-100 leading-none;
   }
 
   .minimal-scrollbar::-webkit-scrollbar {

commit 3c0f14aa08049970f829507d8de5b4dd85a6271b

fix: regression from fdb64aa049e37ee0128942d0e21ca3982e1d28ac which caused gallery close button to be behind the title

src/components/ui/MobileDialog.tsx
@@ -23,14 +23,14 @@ export const DialogContent = React.forwardRef<any, Props>(
           className={clx(fullScreen ? 'dialog-wide' : 'dialog-default')} {...props} ref={forwardedRef}
         >
           <div className='flex items-center px-2'>
+            <DialogPrimitive.Title className='dialog-title'>
+              {title}
+            </DialogPrimitive.Title>
             <DialogPrimitive.Close aria-label='Close' asChild className='dialog-close-button'>
               <button className='btn btn-circle btn-ghost outline-none'>
                 <XMarkIcon size={24} />
               </button>
             </DialogPrimitive.Close>
-            <DialogPrimitive.Title className='dialog-title'>
-              {title}
-            </DialogPrimitive.Title>
           </div>
           {children}
         </DialogPrimitive.Content>