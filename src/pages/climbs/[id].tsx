c4ec1ad54459dd96fef7e3f954f0853fb79cbb31
Julian Lam <julian@nodebb.org>
2024-10-30 23:06:35 -0400

    fix: make this work for boulders too

--- src/pages/climbs/[id].tsx
+++ src/pages/climbs/[id].tsx
@@ -440,7 +440,7 @@ const ClimbData: React.FC<{
 
         {!editMode && (
           <div className='mt-8'>
-            <TickButton climbId={climbId} name={name} grade={gradesObj.toStringTradSportAid()} />
+            <TickButton climbId={climbId} name={name} grade={gradesObj.toString()} />
           </div>
         )}
       </div>
